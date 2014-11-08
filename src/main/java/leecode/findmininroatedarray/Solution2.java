0b4d500ac4529736f13c9cbd3092a42584c865c1
boliu <liubo76@yahoo.com>
2014-11-08 15:43:11 -0800

    solution2

---
+++ src/main/java/leecode/findmininroatedarray/Solution2.java
@@ -0,0 +1,21 @@
+package leecode.findmininroatedarray;
+
+public class Solution2 {
+    public int findMin(int[] num) {
+        if (num == null || num.length == 0) {
+            throw new IllegalArgumentException("num is empty or null");
+        }
+        int start = 0;
+        int end = num.length-1;
+        while (start < end && num[start] > num[end]) {
+            int mid = (start + end ) / 2;
+            if (num[start] > num[mid]) {
+                end = mid;
+            } else {
+                start = start + 1;
+            }
+        }
+        return num[start];
+        
+    }
+}
---
+++ src/main/java/leecode/findminroratearray2/Solution2.java
@@ -0,0 +1,25 @@
+package leecode.findminroratearray2;
+
+/*
+ * as long as left >= right, that mean the array is rotated
+ */
+public class Solution2 {
+    public int findMin(int[] num) {
+        if (num == null || num.length == 0) {
+            throw new RuntimeException("input array is null or empty") ;
+        }
+        int left = 0;
+        int right = num.length-1;
+        while (left < right && num[left] >= num[right]) {
+            int mid = (left + right) / 2;
+            if ( num[left] > num[mid]) {
+                right = mid;
+            } else if ( num[left] < num[mid] ) {
+                left = mid + 1;
+            } else {
+                left = left + 1;
+            }
+        }
+        return num[left];
+    }
+}
