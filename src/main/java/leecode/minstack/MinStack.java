db4cd3fa492e2c9f0e128cdf72c469339279cf59
boliu <liubo76@yahoo.com>
2014-11-10 20:57:32 -0800

    add new

---
+++ src/main/java/leecode/minstack/MinStack.java
@@ -0,0 +1,44 @@
+package leecode.minstack;
+
+import java.util.Stack;
+
+public class MinStack {
+    static class Mark {
+        private int value;
+        private int count;
+        public Mark(int v, int c) {
+            value = v;
+            count = c;
+        }
+    }
+    private Stack<Integer> data = new Stack<>();
+    private Stack<Mark> min = new Stack<>();
+    
+    public void push(int x) {
+        data.push(x);
+        if (min.isEmpty() || x < getMin()) {
+            min.push(new Mark(x,1));
+        } else if (x == getMin()) {
+            min.peek().count = min.peek().count+1;
+        }
+    }
+
+    public void pop() {
+        int value = data.pop();
+        if (value == getMin()) {
+            min.peek().count = min.peek().count - 1;
+            if (min.peek().count == 0) {
+                min.pop();
+            }
+        }
+    }
+
+    public int top() {
+        return data.peek();
+    }
+
+    public int getMin() {
+        return min.peek().value;
+    }
+}
+
---
+++ src/main/java/leecode/minstack/Solution2.java
@@ -0,0 +1,41 @@
+package leecode.minstack;
+
+import java.util.Stack;
+
+public class Solution2 {
+	private Stack<Long> data = new Stack<>();
+	private long min;
+
+	public void push(int x) {
+		if (data.isEmpty()) {
+			data.push(0L);
+			min = x;
+		} else {
+			data.push(x - min);
+			if (x < min) {
+				min = x;
+			}
+		}
+	}
+
+	public void pop() {
+		long value = data.pop();
+		if (value < 0) {
+			min = min - value;
+		}
+	}
+
+	public int top() {
+		long value = data.peek();
+		if (value > 0) {
+			return (int) (value + min);
+		} else {
+			return (int) min;
+		}
+	}
+
+	public int getMin() {
+		return (int) min;
+	}
+
+}
