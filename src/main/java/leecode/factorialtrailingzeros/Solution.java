dd6cddb9eabbbfb11034407c5946331c8ca50a32
boliu <liubo76@yahoo.com>
2014-12-29 21:39:59 -0800

    new question

---
+++ src/main/java/leecode/factorialtrailingzeros/Solution.java
@@ -0,0 +1,11 @@
+package leecode.factorialtrailingzeros;
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int total = 0;
+        for (int i = 5; n/i > 0; i = i * 5) {
+            total += n/i;
+        }
+        return total;
+    }
+}
---
+++ src/main/java/leecode/titletonumber/Solution.java
@@ -0,0 +1,16 @@
+package leecode.titletonumber;
+
+public class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            result = result * 26;
+            result = result + (c - 'A'  + 1);
+        }
+        return result;
+    }
+}
