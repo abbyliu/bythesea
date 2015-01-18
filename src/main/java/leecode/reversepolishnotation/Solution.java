ac2c909f4f0d9aa071835b79f2e10a2e995595ad
boliu <liubo76@yahoo.com>
2015-01-18 14:40:16 -0800

    af

--- src/main/java/leecode/reversepolishnotation/Solution.java
+++ src/main/java/leecode/reversepolishnotation/Solution.java
@@ -1,6 +1,8 @@
 package leecode.reversepolishnotation;
 
+import java.util.Deque;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.Stack;
 
@@ -17,55 +19,6 @@ Valid operators are +, -, *, /. Each operand may be an integer or another expres
 Some examples:
  */
 public class Solution {
-	
-    public int evalRPN2(String[] tokens) {
-        if (tokens == null|| tokens.length == 0) return 0;
-        Stack<Integer> operands = new Stack<>();
-        boolean valid = true;
-        for (int i = 0; i < tokens.length && valid; i++) {
-          String str = tokens[i];
-          if (isInteger2(str)) {
-              operands.push(Integer.parseInt(str));
-          } else {
-              valid = checkOperants(2, operands);
-              if (valid) {
-                  int v2 = operands.pop();
-                  int v1 = operands.pop();
-                  switch(str) {
-                     case "+": operands.push(v1+v2);
-                               break;
-                     case "-": operands.push(v1-v2);
-                               break;
-                     case "*" : operands.push(v1*v2);
-                               break;
-                     case "/": operands.push(v1/v2);
-                               break;
-                     default:  valid = false;
-                  }
-              }
-         }
-        }
-        if (valid && operands.size() == 1) {
-            return operands.pop();
-        } else {
-           return 0;
-        }
-      }
-	
-    private boolean checkOperants(int expected, Stack<Integer> operands) {
-        return operands.size() >= expected;
-    }
-    private boolean isInteger2(String s) {
-        boolean result = false;
-        try {
-            Integer.parseInt(s);
-            result = true;
-        } catch(NumberFormatException e) {
-            ;// ignore
-        }
-        return result;
-    }
-    
 	
 	   private static Set<String> operators = new HashSet<String>();
 	   static {
@@ -122,6 +75,38 @@ public class Solution {
 		   }
 		   return result;
 	   }
+	   
+	   // fastest
+	    public int evalRPN2(String[] tokens) {
+	        if (tokens == null || tokens.length ==0) {
+	            return 0;
+	        }
+	        
+	        Deque<Integer> params = new LinkedList<>();
+	        
+	        for (int i = 0; i < tokens.length; i++ ) {
+	                switch(tokens[i].trim()) {
+	                    case "*": params.push(params.pop() * params.pop()); 
+	                              break;
+	                    case "+": params.push(params.pop() + params.pop());
+	                              break;
+	                    case "-": params.push(-1 * params.pop() + params.pop());
+	                              break;
+	                    case "/": int v = params.pop();
+	                              params.push(params.pop()/v);
+	                              break;
+	                    default:  params.push(new Integer(tokens[i]));
+	                              break;
+	                }
+	        }
+	        
+	        if (params.size() == 1) {
+	            return params.pop();
+	        } else {
+	            throw new RuntimeException("operator not enough");
+	        }
+	        
+	    }   
 	   
 	   public static void main(String[] args) {
 		   String[] input = {"2", "1", "+", "3", "*"};
