ecea41ad4d72ec3e812bf27755dd2259cccb5df4
boliu <liubo76@yahoo.com>
2015-01-18 19:35:16 -0800

    1

--- src/main/java/leecode/interleavingstring/Solution2.java
+++ src/main/java/leecode/interleavingstring/Solution2.java
@@ -1,5 +1,17 @@
 package leecode.interleavingstring;
+/* Interleaving String
+ * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
 
+For example,
+Given:
+s1 = "aabcc",
+s2 = "dbbca",
+
+When s3 = "aadbbcbcac", return true.
+When s3 = "aadbbbaccc", return false. 
+
+O(m*n), O(m) or O(n) after change
+ */
 public class Solution2 {
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s1 == null || s2 == null || s3 == null) return false;
--- src/main/java/leecode/uniquebst/Solution.java
+++ src/main/java/leecode/uniquebst/Solution.java
@@ -1,5 +1,5 @@
 package leecode.uniquebst;
-/*
+/* Unique Binary Search Trees
  * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
  * O(N2)
  */
